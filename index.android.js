654bd5ab4dcf8982f0bf5aa3bc8a4fc569d00eaa
JinHui <1004260403@qq.com>
2017-09-12 15:54:45 +0800

    官方refreshControl

--- index.android.js
+++ index.android.js
@@ -4,91 +4,143 @@
  * @flow
  */
 
-import React, {Component} from 'react';
+/**
+ * RefreshControl
+ * 支持我们
+ * 这一组件可以用在ScrollView或ListView内部，为其添加下拉刷新的功能。
+ * 当ScrollView处于竖直方向的起点位置（scrollY: 0），此时下拉会触发一个onRefresh事件。
+ *
+ * 属性
+ * onRefresh function
+ * 在视图开始刷新时调用。
+ * refreshing bool
+ * 视图是否应该在刷新时显示指示器。
+ * android colors [ColorPropType]
+ * 指定至少一种颜色用来绘制刷新指示器。
+ * android enabled bool
+ * 指定是否要开启刷新指示器。
+ * android progressBackgroundColor ColorPropType
+ * 指定刷新指示器的背景色。
+ * android size RefreshLayoutConsts.SIZE.DEFAULT
+ * 指定刷新指示器的大小，具体数值可参阅RefreshControl.SIZE.
+ * android progressViewOffset React.PropTypes.number
+ * 指定刷新指示器的垂直起始位置(top offset)。
+ * ios tintColor ColorPropType
+ * 指定刷新指示器的颜色。
+ * ios title string
+ * 指定刷新指示器下显示的文字。
+ */
+import React from 'react';
 import {
     AppRegistry,
+    ScrollView,
     StyleSheet,
-    Text,
-    View,
     RefreshControl,
-    ScrollView
+    Text,
+    TouchableWithoutFeedback,
+    View,
 } from 'react-native';
 
-class ListRowComponent extends React.Component{
-    render(){
-        return (
-            <View style={styles.row}>
-              <Text style={styles.text}>
-                  {this.props.data.text}
-              </Text>
-            </View>
-        );
-    }
-}
+const styles = StyleSheet.create({
+    row: {
+        borderColor: 'grey',
+        borderWidth: 1,
+        padding: 20,
+        backgroundColor: '#3a5795',
+        margin: 5,
+    },
+    text: {
+        alignSelf: 'center',
+        color: '#fff',
+    },
+    scrollview: {
+        flex: 1,
+    },
+});
 
-export default class RN_RefreshControl extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
+const Row = React.createClass({
+    _onClick: function () {
+        this.props.onClick(this.props.data);
+    },
+    render: function () {
+        return (
+            <TouchableWithoutFeedback onPress={this._onClick}>
+                <View style={styles.row}>
+                    <Text style={styles.text}>
+                        {this.props.data.text + ' (' + this.props.data.clicks + ' clicks)'}
+                    </Text>
+                </View>
+            </TouchableWithoutFeedback>
+        );
+    },
+});
+
+const RefreshControlExample = React.createClass({
+    statics: {
+        title: '<RefreshControl>',
+        description: 'Adds pull-to-refresh support to a scrollview.'
+    },
+
+    // 初始状态
+    getInitialState() {
+        return {
             isRefreshing: false,
             loaded: 0,
-            rowData: Array.from(new Array(10)).map((val, i) => ({text: '初次加载的数据行 ' + i})),
+            rowData: Array.from(new Array(20)).map(
+                (val, i) => ({text: '初始数据 ' + i, clicks: 0})),
         };
-    }
+    },
+    // 点击事件
+    _onClick(row) {
+        row.clicks++;
+        this.setState({
+            rowData: this.state.rowData,
+        });
+    },
 
     render() {
-        var rows = this.state.rowData.map((row, indexKey) => {
-            return <ListRowComponent key={indexKey} data={row}/>;
+        const rows = this.state.rowData.map((row, ii) => {
+            return <Row key={ii} data={row} onClick={this._onClick}/>;
         });
         return (
             <ScrollView
                 style={styles.scrollview}
                 refreshControl={
-                  <RefreshControl
-                      refreshing={this.state.isRefreshing}
-                      onRefresh={this.onRefresh.bind(this)}  //(()=>this.onRefresh)或者通过bind来绑定this引用来调用方法
-                      tintColor='red'
-                      title= {this.state.isRefreshing? '刷新中....':'下拉刷新'}
-                  />
+                    <RefreshControl
+                        refreshing={this.state.isRefreshing}
+                        onRefresh={this._onRefresh}
+                        // 指定刷新指示器的颜色
+                        tintColor="#ff0000"
+                        title="Loading..."
+                        titleColor="#00ff00"
+                        colors={['#ff0000', '#00ff00', '#0000ff']}
+                        progressBackgroundColor="#ffff00"
+                    />
                 }>
                 {rows}
             </ScrollView>
         );
-    }
+    },
 
-    onRefresh() {
+    // 加载数据
+    _onRefresh() {
         this.setState({isRefreshing: true});
         setTimeout(() => {
-            // 准备下拉刷新的5条数据
-            var rowNewData = Array.from(new Array(5))
+            // prepend 10 items
+            const rowData = Array.from(new Array(10))
                 .map((val, i) => ({
-                    text: '下拉刷新增加的数据行 ' + (+this.state.loaded + i)
+                    text: '加载数据 ' + (+this.state.loaded + i),
+                    clicks: 0,
                 }))
                 .concat(this.state.rowData);
-            this.setState({
-                loaded: this.state.loaded + 5,
-                isRefreshing: false,
-                rowData: rowNewData,
-            });
-        }, 2000);
-    }
-}
 
-const styles = StyleSheet.create({
-    row: {
-        borderColor:'green',
-        borderWidth:5,
-        padding:20,
-        backgroundColor:'#3a5795',
-        margin:5,
+            this.setState({
+                loaded: this.state.loaded + 10,
+                isRefreshing: false,
+                rowData: rowData,
+            });
+        }, 3000);
     },
-    text:{
-        alignSelf:'center',
-        color:'white',
-    },
-    scrollerview:{
-        flex:1,
-    }
 });
 
-AppRegistry.registerComponent('RN_RefreshControl', () => RN_RefreshControl);
+AppRegistry.registerComponent('RN_RefreshControl', () => RefreshControlExample);
